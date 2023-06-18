8af7dfbb3d1d5292897c70f4e88437064d80ef04
benboydens <r0857165@student.vives.be>
2023-06-18 20:16:15 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -3,51 +3,95 @@ import './App.css';
 import initialize_cards from './api/fetch';
 import { useEffect, useState } from 'react';
 import { Card } from './models/card';
+import { Deck } from './models/deck';
 
-function Deck() {
 
-  const [cards, setCards] = useState(Array<Card>)
-  useEffect(() => {
-    async function getCards() {
-      let all_cards = await initialize_cards();
-      setCards(all_cards);
-    }
-    getCards();
-  })
+function App() {
+  const [deck, setDeck] = useState<Deck>(new Deck());
+  const [cards, setCards] = useState<Array<Card>>(deck.getCards());
+  
+  function handleClick(card: Card) {
+    deck.playCard(card)
+    setDeck(deck);
+    setCards(deck.getCards());
+  }
 
-  const listCards = cards.map(c => <div>{c.name}<img src={c.img} alt={c.name}/></div>);
+  function resetDeck() {
+    console.log("Reset")
+    setDeck(new Deck());
+    setCards(deck.getCards());
+  }
+  
+  let deckList = cards.map((c, i) => <div className={i >= 4 && c.id !== 0 ? "card-deck-darken" : "card-deck"} key={c.id + '-' + i }><PickerCard card={c}/></div>)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Yo dit is een deck bro
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <div>
-          <div>Unknown<img src="/unknown.png" alt="Unknown"/></div>
-          {listCards}
-        </div>
-      </header>
-    </div>
+    <div className="app">
+    <header className="app-header container pt-3">
+      <button className="btn btn-dark float-end" onClick={() => resetDeck()}>Reset</button>
+      <h3>Deck</h3>
+      <div className="deck py-3">
+        {deckList}
+      </div>
+      <hr />
+      <CardList onCardClick={handleClick} />
+    </header>
+  </div>
   );
 }
 
-// function Cardx() {
-//   return (
-//     <div>
-//       <div>Title</div>
-//       <img src="" alt="FUCKYOUIKZETWATIKwiLHIER" />
-//     </div>
-//   );
-// }
 
-export default Deck;
+function CardList({ onCardClick }: { onCardClick: any }) {
+  const [cards, setCards] = useState<Array<Card>>([])
+  const [quickPicks, setQuickPicks] = useState<Array<Card>>([])
+  
+  const getCards = async () => {
+    console.log('GET CARDS');
+    const all_cards = await initialize_cards();
+    setCards(all_cards);
+  }
+
+  function updateCards(card: Card) {
+    onCardClick(card);
+
+    if (quickPicks.length < 8 && quickPicks.find(c => c.id === card.id) === undefined) {
+      setQuickPicks(quickPicks.concat(card));
+    }
+  }
+  
+  useEffect(() => {
+    getCards()
+  }, [])
+
+  const listPicks = quickPicks.map(c => <div className="col-1 card-qp" onClick={() => updateCards(c) } key={c.id}><PickerCard card={c} /></div>);
+  const listCards = cards.filter(c => c.img).map(c => <div className="col-2 col-md-1" onClick={() => updateCards(c) } key={c.id}><PickerCard card={c} /></div>);
+
+  return (
+    <>
+
+      { quickPicks.length > 0 ? 
+      (
+        <>
+          <h3>Quick Pick</h3>
+          <div className="row g-0 py-3">
+            {listPicks}
+          </div>
+          <hr />
+        </>
+      ) : (<></>)
+      }
+      <h3>Cards</h3>
+      <div className="row g-0">
+        {listCards}
+      </div>
+    </>
+  );
+}
+
+
+function PickerCard({ card }: { card: Card }) {
+  return (
+    <img className="card-img" src={card.img} alt={card.name} />
+  );
+}
+
+export default App;
--- src/models/deck.ts
+++ src/models/deck.ts
@@ -1,7 +1,7 @@
 import { Card } from "./card";
 
-class Deck {
-    private first: Card;
+export class Deck {
+    public first: Card;
 
     constructor() {
         // create new deck with empty cards
@@ -49,50 +49,89 @@ class Deck {
         let current: Card = this.first;
         while (current.next !== undefined) {
             if (index < 4) {
-                console.log("\x1b[32m", `Card: ${current.name} - cost: ${current.cost} elixer`);
+                console.log("\x1b[32m", `Card: ${current.name} - cost: ${current.cost} elixer - index: ${index}`);
             } else {
-                console.log("\x1b[31m", `Card: ${current.name} - cost: ${current.cost} elixer`);
+                console.log("\x1b[31m", `Card: ${current.name} - cost: ${current.cost} elixer - index: ${index}`);
             }
             current = current.next;
             index++;
         }
-        console.log("\x1b[31m", `Card: ${current.name} - cost: ${current.cost} elixer`);
+        console.log("\x1b[31m", `Card: ${current.name} - cost: ${current.cost} elixer - index: ${index}`);
     }
 
+    private cardInDeck(card: Card): boolean {
+        let current: Card = this.first;
+        while (current.next !== undefined) {
+            if (current.id === card.id) {
+                return true;
+            }
+            current = current.next;
+        }
 
-    public playCard(card: Card) {
-        console.log("\x1b[0m", `\n==== Play ${card.name} ====\n`)
+        return current.id === card.id;
+    }
 
-        // first check if card exists in first 4 cards
+    private firstUnknownIndex(): number {
         let index: number = 0;
-        let unknown_index: number = -1;
         let current: Card | undefined = this.first;
-        while (index < 4 && current && current.id !== card.id) {
-            if (current && current.id === 0) unknown_index = index;
+        while (index < 4 && current) {
+            if (current.id === 0) {
+                return index;
+            }
             current = current.next;
             index++;
         }
 
-        if (index < 4) {
-            // move existing card to the back of the queue
-            let removed: Card | undefined = this.removeFrom(index);
-            if (!removed) return;
+        return -1;
+    }
 
-            removed.next = undefined;
-            this.addCard(removed);
+    public playCard(card: Card) {
+        // first check if card exists in first 4 cards
+        let index: number = 0;
+        let current: Card | undefined = this.first;
+        while (index < 8 && current && current.id !== card.id) {
+            current = current.next;
+            index++;
+        }
+
+        if (this.cardInDeck(card)) {
+            if (index < 4) {
+                // move existing card to the back of the queue
+                let removed: Card | undefined = this.removeFrom(index);
+                if (!removed) return;
+
+                removed.next = undefined;
+
+                this.addCard(removed);
+            }
         } else {
             // replace unknown card with played card and move to back
+            let unknown_index = this.firstUnknownIndex();
+            if (unknown_index < 0 || unknown_index > 3) return;
+
             let removed: Card | undefined = this.removeFrom(unknown_index);
             if (!removed) return;
 
-            removed.setName(card.name);
-            removed.setCost(card.cost);
+            removed.id = card.id;
+            removed.cost = card.cost;
+            removed.name = card.name
+            removed.img = card.img;
             removed.next = undefined;
 
             this.addCard(removed);
         }
+    }
 
+    public getCards(): Array<Card> {
+        let cards: Array<Card> = []
+        let current: Card = this.first;
+
+        while (current.next !== undefined) {
+            cards.push(current);
+            current = current.next;
+        }
+        cards.push(current);
+
+        return cards;
     }
 }
-
-export default Deck;
